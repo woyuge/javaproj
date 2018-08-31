24613f7ed6a26b717c73bf80b768b2b42f242bc0
zhuzhenyu <zhuzhenyu@globalegrow.com>
2018-08-31 17:15:48 +0800

    新添章节

---
+++ chap1/src/test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by zhuzhenyu on 2018/8/31.
+ */
+public class test {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.print("heloossss");
+    }
+}
